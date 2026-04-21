a94387afb991e16da833fc1c9baada0c22972b6d
Victor Duchesne <victor.ducehsne-delapierre@etu.univ-smb.fr>
2026-04-21 15:05:11 +0200

    Conexion et deconexion en dehors de App.svelte

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -40,7 +40,6 @@
 </script>
 
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import TodoList from './TodoList.svelte';
 	import Header from './Header.svelte';
 	import Conexion from './Conexion.svelte';
@@ -138,52 +137,52 @@
 	$inspect('Tâches chargées :', todos);
 	$inspect('Utilisateurs chargés :', users);
 
-	// Fonction de connexion qui envoie les identifiants à l'API et stocke le token en cas de succès
-	async function login() {
-		try {
-			const response = await fetch(API_URL, {
-				method: 'POST',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify({
-					query: `
-	        mutation($email: String!, $password: String!) {
-			authenticateUserWithPassword(email: $email, password: $password) {
-	            ... on UserAuthenticationWithPasswordSuccess {
-					sessionToken
-				item {
-	                id
-	                name
-	                email
-					}
-	            }
-	            ... on UserAuthenticationWithPasswordFailure {
-					message
-	            }
-			}
-	        }`,
-					variables: { email: email, password: password }
-				})
-			});
+	// // Fonction de connexion qui envoie les identifiants à l'API et stocke le token en cas de succès
+	// async function login() {
+	// 	try {
+	// 		const response = await fetch(API_URL, {
+	// 			method: 'POST',
+	// 			headers: { 'Content-Type': 'application/json' },
+	// 			body: JSON.stringify({
+	// 				query: `
+	//         mutation($email: String!, $password: String!) {
+	// 		authenticateUserWithPassword(email: $email, password: $password) {
+	//             ... on UserAuthenticationWithPasswordSuccess {
+	// 				sessionToken
+	// 			item {
+	//                 id
+	//                 name
+	//                 email
+	// 				}
+	//             }
+	//             ... on UserAuthenticationWithPasswordFailure {
+	// 				message
+	//             }
+	// 		}
+	//         }`,
+	// 				variables: { email: email, password: password }
+	// 			})
+	// 		});
 
-			const result = await response.json();
+	// 		const result = await response.json();
 
-			if (result.errors) {
-				console.error("Détail de l'erreur serveur :", result.errors);
-				alert('Erreur serveur : ' + result.errors[0].message);
-				return;
-			}
+	// 		if (result.errors) {
+	// 			console.error("Détail de l'erreur serveur :", result.errors);
+	// 			alert('Erreur serveur : ' + result.errors[0].message);
+	// 			return;
+	// 		}
 
-			const auth = result.data.authenticateUserWithPassword;
+	// 		const auth = result.data.authenticateUserWithPassword;
 
-			if (auth.sessionToken) {
-				token = auth.sessionToken;
-			} else {
-				alert(auth.message || 'Email ou mot de passe incorrect');
-			}
-		} catch (err) {
-			console.error('Erreur technique :', err);
-		}
-	}
+	// 		if (auth.sessionToken) {
+	// 			token = auth.sessionToken;
+	// 		} else {
+	// 			alert(auth.message || 'Email ou mot de passe incorrect');
+	// 		}
+	// 	} catch (err) {
+	// 		console.error('Erreur technique :', err);
+	// 	}
+	// }
 	// Fonction de déconnexion qui met à jour le token
 	function logout() {
 		token = null;
@@ -261,12 +260,7 @@
 </script>
 
 {#if !isConnected}
-	<div class="login-container">
-		<h2>Connexion</h2>
-		<input type="email" placeholder="Email" bind:value={email} />
-		<input type="password" placeholder="Mot de passe" bind:value={password} />
-		<button onclick={login}>Se connecter</button>
-	</div>
+	<Conexion {API_URL} {email} {password} bind:token bind:me />
 {:else}
 	<div class="app-container">
 		<Header {me} onlogout={logout} />
--- frontend/src/Conexion.svelte
+++ frontend/src/Conexion.svelte
@@ -1,5 +1,51 @@
 <script>
-	let { login, email, password } = $props();
+	let { API_URL, email, password, token = $bindable(), me = $bindable() } = $props();
+
+	async function login() {
+		try {
+			const response = await fetch(API_URL, {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					query: `
+	        mutation($email: String!, $password: String!) {
+			authenticateUserWithPassword(email: $email, password: $password) {
+	            ... on UserAuthenticationWithPasswordSuccess {
+					sessionToken
+				item {
+	                id
+	                name
+	                email
+					}
+	            }
+	            ... on UserAuthenticationWithPasswordFailure {
+					message
+	            }
+			}
+	        }`,
+					variables: { email: email, password: password }
+				})
+			});
+
+			const result = await response.json();
+
+			if (result.errors) {
+				console.error("Détail de l'erreur serveur :", result.errors);
+				alert('Erreur serveur : ' + result.errors[0].message);
+				return;
+			}
+
+			const auth = result.data.authenticateUserWithPassword;
+
+			if (auth.sessionToken) {
+				token = auth.sessionToken;
+			} else {
+				alert(auth.message || 'Email ou mot de passe incorrect');
+			}
+		} catch (err) {
+			console.error('Erreur technique :', err);
+		}
+	}
 </script>
 
 <div class="login-container">
